f566e40109e23fa4535b3d6673856ffcbcbfbde6
Jaime Cueva Sanchez <jaime.cueva@ibm.com>
2020-04-11 22:37:39 -0500

    a

--- chaincode-arbol/src/arbol-contract.ts
+++ chaincode-arbol/src/arbol-contract.ts
@@ -47,6 +47,7 @@ export class ArbolContract extends Contract {
             const arbolesLength = (arboles.length + 1).toString();
             const arbolId = 'ARBOL' + arbolesLength.padStart(10, '0000000000');
             arbol.id = arbolId;
+            arbol.trozado = false;
             const buffer = Buffer.from(JSON.stringify(arbol));
             await ctx.stub.putState(arbolId, buffer);
         } else {
